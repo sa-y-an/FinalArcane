202f5108d10c8e6a9754dcac2c9147891923ec1e
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-12-23 11:19:32 +0530

    Update views.py

--- quiz/views.py
+++ quiz/views.py
@@ -26,11 +26,11 @@ now = datetime.utcnow()+timedelta(hours=5.5)
 
 
 #Start
-quiz = datetime(2021, 10, 23, 19, 0, 0)       
+quiz = datetime(2021, 12, 25, 15, 0, 0)       
 # First Round Ends
-firstend = datetime(2022, 10, 24, 19, 0, 0)
+firstend = datetime(2022, 10, 26, 6, 0, 0)
 # Second Round Ends
-end = datetime(2022, 10, 25, 23, 59, 59)
+end = datetime(2022, 10, 26, 18, 0, 0)
 
 
 
